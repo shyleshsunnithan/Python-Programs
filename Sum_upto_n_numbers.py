4b6f4021d6ef9eb582af153d76268d9c15de359e
Shylesh S <37534898+shyleshsunnithan@users.noreply.github.com>
2019-07-04 02:36:25 +0530

    Update Sum_upto_n_numbers.py

--- Sum_upto_n_numbers.py
+++ Sum_upto_n_numbers.py
@@ -1,6 +1,6 @@
 
 
-def recur_sum(n):
+def recur_sum(n):                                     #  recursive function
 
    if n <= 1:
        return n
@@ -8,12 +8,12 @@ def recur_sum(n):
        return n + recur_sum(n-1)
 
 
-num=int(input("Enter The Number: "))
+num=int(input("Enter The Number: "))                  #  read limit
 
 
 
 
-if num < 0:
+if num < 0:                                           #  check whether the number is positive
    print("Enter a positive number")
 else:
-   print("The sum is: ",recur_sum(num))
+   print("The sum is: ",recur_sum(num))               #  print sum
