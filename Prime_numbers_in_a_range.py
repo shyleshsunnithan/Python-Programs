3cf56d4702ce4eeadc6dc56e816ecf56e7e8b249
Shylesh S <37534898+shyleshsunnithan@users.noreply.github.com>
2020-03-08 23:58:56 +0530

    Update Prime_numbers_in_a_range.py

--- Prime_numbers_in_a_range.py
+++ Prime_numbers_in_a_range.py
@@ -6,7 +6,7 @@ for num in range(lower,upper + 1):
 
    if num > 1:
        for i in range(2,num): #for loop initialized
-           if (num % i) == 0:
+           if (num % i) == 0: #checkin for prime
                break
        else:
            print(num) #print prime number
